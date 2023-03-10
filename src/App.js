ac9a9f2f06235797ef2464f655a25f77abb4383f
Smith Ringtho <sringtho@gmail.com>
2023-03-10 06:16:10 +0000

    Added React Router DOM package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Smith Ringtho</h1>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route/>
+        </Routes>
+
+      </BrowserRouter>
+      
     </div>
   );
 }
