4a85e39ee9eea1e52939579e4a8312443988c95d
Smith Ringtho <sringtho@gmail.com>
2023-03-10 07:31:15 +0000

    Added miragejs to allow for a dummy api creation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout';
 import Home from "./pages/Home"
 import About from './pages/About'
 import Vans from './pages/Vans'
+import "./server"
 
 function App() {
   return (
--- src/pages/Vans.js
+++ src/pages/Vans.js
@@ -1,8 +1,19 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
 
 export default function Vans(){
+
+    const [vans , setVans] = useState([])
+
+    useEffect(()=>{
+        fetch('/api/vans')
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    },[])
+
+    console.log(vans)
+
     return (
-        <h1>Vans</h1>
+        <h1>Explore our Van options</h1>
     )
 }
