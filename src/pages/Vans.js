3b1fd3ea998103753cb11820b3bd6f43e2f47099
Smith Ringtho <sringtho@gmail.com>
2023-03-10 09:05:05 +0000

    Added ability to filter type using links from the type buttons

--- src/pages/Vans.js
+++ src/pages/Vans.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react"
-import { useSearchParams } from "react-router-dom"
+import { useSearchParams, Link } from "react-router-dom"
 
 
 export default function Vans(){
@@ -21,20 +21,31 @@ export default function Vans(){
     vans.filter(van => van.type === typeFilter.toLocaleLowerCase()) :
     vans
 
+    
+
     const vansEl = displayVans.map((van) => {
         return (
+            <>
+            
+            
             <div className="van" key={van.id}>
                 <img src={van.imageUrl} alt={van.name} className="van-img"/>
                 <h3>{van.name}</h3>
                 <p>${van.price} /day</p>
-                <span className="van-type">{van.type}</span>
+                <span className={`van-type ${van.type}`}>{van.type}</span>
             </div>
+            </>
         )
     })
 
     return (
         <>
         <h1>Explore our Van options</h1>
+        <div className="van-filter">
+            <Link to="?type=simple" className="van-type simpleBtn">Simple</Link>
+            <Link to="?type=luxury" className="van-type luxuryBtn">Luxury</Link>
+            <Link to="?type=rugged" className="van-type ruggedBtn">Rugged</Link>
+        </div>
         {vansEl}
         </>
     )
