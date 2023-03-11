6f7f1283c7462d765ad771ae9f5b7b97e42c6652
Smith Ringtho <sringtho@gmail.com>
2023-03-11 23:45:17 +0300

    Updated Error message object in api.js

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,14 +1,16 @@
 
 
+
 export async function getVans(id){
     const url = id ? `/api/vans/${id}` : '/api/vans'
     const res = await fetch(url)
+    const errorMessage = {
+        message: "Failed to fetch vans",
+        statusText: res.statusText,
+        status: res.status
+    }
     if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
+        throw errorMessage
     }
     const data = await res.json()
     return data.vans 
