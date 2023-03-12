0a32004a5b5a724ef301ae5f8ab47289580c88bf
Smith Ringtho <sringtho@gmail.com>
2023-03-12 10:07:40 +0300

    Added asynchronous rendering of van detail vans using defer and Await

--- src/pages/vans/VanDetail.js
+++ src/pages/vans/VanDetail.js
@@ -1,32 +1,39 @@
-import { Link, useLocation, useLoaderData } from "react-router-dom" 
+import { Suspense } from "react"
+import { Link, useLocation, useLoaderData, defer, Await } from "react-router-dom" 
 import { getVans } from "../api"
 
 export function loader({params}){
-    return getVans(params.id)
+    return defer({van : getVans(params.id)})
 }
 
-
 export default function VanDetail(){
-    const van = useLoaderData()
+    const vanPromise = useLoaderData()
     const location = useLocation()
-
     const backPath = location.state?.filter  || ""
     const vanType = location.state?.type || "All"
+
+    const renderVanData = (van) => {
+        return (
+            <>
+            <img src={van.imageUrl} alt={van.name} className="van-detail-img" />
+            <span className={`van-type ${van.type}`}>{van.type}</span>
+            <h1 className="van-name">{van.name}</h1>
+            <h2>${van.price} /day</h2>
+            <p>{van.description}</p>
+            <button className="rentBtn">RENT THIS VAN</button>
+            </>
+        )
+    }
 
     return(
         <div className="van-detail-wrapper">
             <Link to={`..${backPath}`} relative="path"
             >&larr; Back to {vanType} Vans</Link>
-
-            { van ?(
-            <>
-                <img src={van.imageUrl} alt={van.name} className="van-detail-img" />
-                <span className={`van-type ${van.type}`}>{van.type}</span>
-                <h1 className="van-name">{van.name}</h1>
-                <h2>${van.price} /day</h2>
-                <p>{van.description}</p>
-                <button className="rentBtn">RENT THIS VAN</button>
-            </>) : <h2>Loading..</h2> }
+            <Suspense fallback={<h2>Loading...</h2>}>
+                <Await resolve={vanPromise.van}>
+                    {renderVanData}
+                </Await>
+            </Suspense>
         </div>
         
     )
