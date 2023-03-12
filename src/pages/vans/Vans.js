884bc25d3c84cc53864c6f9971ece119e1768162
Smith Ringtho <sringtho@gmail.com>
2023-03-12 10:16:44 +0300

    using defer in the loader, Await and suspense to synchronously render vans to the vans page with a loading status

--- src/pages/vans/Vans.js
+++ src/pages/vans/Vans.js
@@ -1,43 +1,23 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { 
     useSearchParams, 
     useLocation, 
     Link, 
-    useLoaderData
+    useLoaderData, 
+    defer,
+    Await
 } from "react-router-dom"
 import { getVans } from "../api"
 
 export function loader(){
-    return getVans()
+    return defer({vans : getVans()})
 }
 
 export default function Vans(){
-    const vans = useLoaderData()
+    const vansPromise = useLoaderData()
     const [searchParams] = useSearchParams()
     const location = useLocation()
     const typeFilter = searchParams.get("type")
-
-    const displayVans = typeFilter 
-    ? vans.filter(van => van.type === typeFilter.toLocaleLowerCase()) 
-    : vans
-
-    const vansEl = displayVans.map((van) => {
-        return (
-            <Link 
-                to={van.id} 
-                key={van.id} 
-                className="van-link"
-                state={{type: typeFilter, filter: location.search}}
-            >
-                <div className="van" key={van.id}>
-                    <img src={van.imageUrl} alt={van.name} className="van-img"/>
-                    <h3>{van.name}</h3>
-                    <p>${van.price} /day</p>
-                    <span className={`van-type ${van.type}`}>{van.type}</span>
-                </div>
-            </Link>
-        )
-    })
 
     function createSearchParamString(key, value){
         const sp = new URLSearchParams(searchParams)
@@ -50,10 +30,31 @@ export default function Vans(){
         return `?${sp.toString()}`
     }
 
-    return (
-        <>
-        <h1>Explore our Van options</h1>
-        <div className="van-filter">
+    function renderVans(vans){
+        const displayVans = typeFilter 
+        ? vans.filter(van => van.type === typeFilter.toLocaleLowerCase()) 
+        : vans
+    
+        const vansEl = displayVans.map((van) => {
+            return (
+                <Link 
+                    to={van.id} 
+                    key={van.id} 
+                    className="van-link"
+                    state={{type: typeFilter, filter: location.search}}
+                >
+                    <div className="van" key={van.id}>
+                        <img src={van.imageUrl} alt={van.name} className="van-img"/>
+                        <h3>{van.name}</h3>
+                        <p>${van.price} /day</p>
+                        <span className={`van-type ${van.type}`}>{van.type}</span>
+                    </div>
+                </Link>
+            )
+        })
+        return (
+            <>
+            <div className="van-filter">
             <Link 
                 to={createSearchParamString("type", "simple")} 
                 className={`van-type simpleBtn 
@@ -72,6 +73,18 @@ export default function Vans(){
             {typeFilter && <Link to="." className="van-type">Clear Filter</Link>}
         </div>
         {vansEl}
+        </>
+        )
+    }
+
+    return (
+        <>
+        <h1>Explore our Van options</h1>
+        <Suspense fallback={<h2>Loading...</h2>}>
+        <Await resolve={vansPromise.vans}>
+        {renderVans}
+        </Await>
+        </Suspense>
         </>
     )
 }
