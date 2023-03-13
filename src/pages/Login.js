2abe9f2342a2f8f8956038194b7d93a4d9729f9f
Smith Ringtho <sringtho@gmail.com>
2023-03-13 06:32:03 +0000

    Added login form and added state for controlling the controlled components and handleSubmit state

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,40 @@
-import React from "react"
+import React, {useState} from "react"
 
 
 export default function Login(){
+
+    const [formData, setFormData] = useState({
+        email: "", password: ""
+    })
+
+    function handleChange(e){
+        setFormData(prev => (
+            {...prev, [e.target.name] : e.target.value}
+        ))
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(formData)
+    }
+
     return (
-        <form className="login-wrapper">
-            <h3>SIGN IN TO YOUR ACCOUNT</h3>
-            <input type="text" placeholder="Enter Email Addresses" />
-            <input type="password" placeholder="Enter Password" />
+        <form className="login-wrapper" onSubmit={handleSubmit}>
+            <h3>Sign in to your account</h3>
+            <input 
+                type="text" 
+                placeholder="Enter Email Addresses" 
+                name="email"
+                value={formData.email}
+                onChange={handleChange} 
+            />
+            <input 
+                type="password" 
+                placeholder="Enter Password" 
+                name="password"
+                value={formData.password}
+                onChange={handleChange} 
+            />
             <button>Log in</button>
         </form>
     )
